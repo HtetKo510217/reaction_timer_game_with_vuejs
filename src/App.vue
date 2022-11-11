4f194b890674240b67f9011035bdc6a1b99340b8
htetko <htetko.connetis@gmail.com>
2022-11-11 21:51:48 +0630

    Create Block component and show block component click play btn within random time

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <p>Reaction timer Game</p>
+    <button @click="start" class="playBtn">Play</button>
+    <div v-if="isPlaying">
+      <Block />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Block from './components/Block.vue'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components : {
+    Block,
+  },
+  data() {
+    return {
+      isPlaying : false,
+      timer:null,
+    }
+  },
+  methods: {
+    start() {
+      this.timer = 1000+ Math.random() * 4000;
+      setTimeout(()=> {
+        this.isPlaying = true;
+      },this.timer)
+    }
   }
 }
 </script>
@@ -22,5 +40,16 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.playBtn {
+  cursor: pointer;
+  width: 80px;
+  background: darkgray;
+  border: none;
+  padding: 10px;
+  border-radius: 10px;
+  color: darkred;
+  font-weight: bold;
+  font-size: 16px;
 }
 </style>
---
+++ src/components/Block.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="block">
+    I am block
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.block {
+    width : 300px;
+    height: 300px;
+    box-sizing: border-box;
+    background: darkred;
+    color: #fff;
+    padding: 10px;
+    margin: 10px auto;
+}
+</style>
